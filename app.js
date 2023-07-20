0754ffdb026149211874fc2d343e49d553592653
Kim Tony <tony@Kimui-MacBookPro.local>
2023-07-20 23:08:20 +0900

    fix: feature/로그인_POST_METHOD와 충돌

--- app.js
+++ app.js
@@ -3,20 +3,12 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const login = require("./routes/loginRoutes");
-const presentation = require("./routes/presentationRoutes");
-const slide = require("./routes/slideRoutes");
-
 const app = express();
 mongoose.connect(process.env.DB_URI);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
-
-app.use("/login", login);
-app.use("/users/:user_id/presentations", presentation);
-app.use("/users/:user_id/presentations/:presentation_id/slides", slide);
 
 app.use((req, res, next) => {
   next(createError(404));
