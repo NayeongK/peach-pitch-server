120538ffea1670c88db422e040a6455955f64d70
Kim Tony <tony@Kimui-MacBookPro.local>
2023-07-26 02:29:24 +0900

    fix: 오타수정

--- controllers/presentationController.js
+++ controllers/presentationController.js
@@ -25,15 +25,13 @@ async function getPresentation(req, res, next) {
   const { presentation_id } = req.params;
 
   try {
-    const presentation = await Presentation.findById({ presentation_id });
+    const presentation = await Presentation.findById(presentation_id);
 
     if (!presentation) {
-      return res
-        .status(404)
-        .json({
-          result: "error",
-          message: `No presentation found with ${presentation_id}`,
-        });
+      return res.status(404).json({
+        result: "error",
+        message: `No presentation found with ${presentation_id}`,
+      });
     }
 
     res.json({ result: "success", presentation });
